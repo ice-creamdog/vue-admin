a015da54e862e02d47740aad641c2f7937b6ce1b
Derek Moment <1329467963@qq.com>
2020-10-02 15:15:18 +0800

    项目构建，全局样式，路由，elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div id="login">
+    登陆页面
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#login {
+    background-color: #344a5f;
+}
+</style>
